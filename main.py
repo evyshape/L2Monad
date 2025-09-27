f1c56650a79ec738fcff2cbe40e2777f5d62f544
evyshape <219411300+evyshape@users.noreply.github.com>
2025-09-27 16:13:29 +0300

    fix russian usernames in windows

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import sys
 import os
-import getpass
 from pathlib import Path
 import asyncio
-
+import PyQt5
 from PyQt5.QtWidgets import QApplication
 from qasync import QEventLoop
 
@@ -19,13 +18,10 @@ async def main():
     gui.show()
 
 if __name__ == "__main__":
-    user = getpass.getuser()
-    p = Path(sys.prefix) / "Lib" / "site-packages" / "PyQt5" / "Qt5" / "plugins" / "platforms"
-
-    if any(ord(c) > 127 for c in user):
-        # костыль для челов с ру символами в юзере, ну че поделать
-        # либо так, либо запускать из пути где нет кирилицы
-        os.environ["QT_QPA_PLATFORM_PLUGIN_PATH"] = str(p)
+    base = Path(PyQt5.__file__).resolve().parent
+    plugs = base / "Qt5" / "plugins" / "platforms"
+    
+    os.environ["QT_QPA_PLATFORM_PLUGIN_PATH"] = str(plugs)
 
     kb, m = checkDriver()
     app = QApplication(sys.argv)
